f507b40f50dc010c23b4bfd7e777cb11af34aba1
Axalz <87911548+Axalz@users.noreply.github.com>
2024-11-26 00:15:16 +0100

    Update loader.lua

--- 3.0/loader.lua
+++ 3.0/loader.lua
@@ -267,7 +267,7 @@ togglebutton.Text = ""
 togglebutton.TextColor3 = Color3.fromRGB(0, 0, 0)
 togglebutton.TextSize = 14.000
 togglebutton.MouseButton1Click:Connect(function()
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/deaddigi/pepsi.club/main/3.0/main.lua", true))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Axalz/pepsi.club/main/3.0/main.lua", true))()
 	game.CoreGui.loader:Destroy()
 end)
 
